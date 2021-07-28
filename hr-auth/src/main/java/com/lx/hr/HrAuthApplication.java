28853ef15f4e46398d02c3ccdfcda1098e0f74e6
wangyouzheng <wangyouzheng@lianxin-tech.com>
2021-07-28 17:43:33 +0800

    分离鉴权独立服务。

---
+++ hr-auth/src/main/java/com/lx/hr/HrAuthApplication.java
@@ -0,0 +1,16 @@
+package com.lx.hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: WangYouzheng
+ * @Date: 2021/7/28 17:42
+ * @Description:
+ */
+@SpringBootApplication
+public class HrAuthApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HrAuthApplication.class, args);
+	}
+}
